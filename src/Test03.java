d465d26e34bd89bd483a82f1d579c1f4addd8682
WYFCN <email@example.com>
2020-07-12 01:17:43 +0800

    232323

--- src/Test03.java
+++ src/Test03.java
@@ -1,11 +1,19 @@
 
 public class Test03 {
     public static void main(String[] args) {
+<<<<<<< HEAD
         int ans=0;
         for(int i=0;i<10;i++)
         {
             ans+=i;
         }
         System.out.println(ans);
+=======
+        System.out.println("556af5asfas56f1a6f1a");
+        System.out.println("556af5asfas56f1a6f1a");
+        System.out.println("556af5asfas56f1a6f1a");
+        System.out.println("556af5asfas56f1a6f1a");
+        System.out.println("556af5asfas56f1a6f1a");
+>>>>>>> Sprint_666_12
     }
 }
