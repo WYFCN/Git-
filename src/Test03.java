caec865a76e1a67ed7d71dd6c860540aeafe3fbd
WYFCN <email@example.com>
2020-07-11 23:43:38 +0800

    最后一次更细

--- src/Test03.java
+++ src/Test03.java
@@ -1,10 +1,6 @@
 
 public class Test03 {
     public static void main(String[] args) {
-        int p=0;
-        do {
-            System.out.println("w");
-            p++;
-        }while (p<20);
+        System.out.println("什么都没有");
     }
 }
