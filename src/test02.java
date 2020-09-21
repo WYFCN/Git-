c2bf21c87a022b8251235b5b8c23e8ae430ca98b
WYFCN <email@example.com>
2020-09-21 20:12:11 +0800

    第二天

--- src/test02.java
+++ src/test02.java
@@ -4,24 +4,6 @@ import java.util.*;
 
 public class test02{
     public static void main(String[] args) {
-        long a=2,b=3;
-        System.out.println(quickmod(a,b,10000000));
+        System.out.println(quickmod("第二天");
     }
-    public static long quickmod(long a,long b,long m)
-    {
-        long ans = 1;
-        a%=m;
-        while(b>0)
-        {
-            if(b%2!=0)
-            {
-                ans = (ans*a)%m;
-                b--;
-            }
-            b/=2;
-            a = a*a%m;
-        }
-        return ans;
-    }
-
 }
