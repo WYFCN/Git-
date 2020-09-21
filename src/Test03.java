101c29a936f92839e731bd29a2f5c65ca55c6adb
WYFCN <email@example.com>
2020-09-21 19:56:35 +0800

    第一天

--- src/Test03.java
+++ src/Test03.java
@@ -1,19 +1,6 @@
 
 public class Test03 {
     public static void main(String[] args) {
-<<<<<<< HEAD
-        int ans=0;
-        for(int i=0;i<10;i++)
-        {
-            ans+=i;
-        }
-        System.out.println(ans);
-=======
-        System.out.println("556af5asfas56f1a6f1a");
-        System.out.println("556af5asfas56f1a6f1a");
-        System.out.println("556af5asfas56f1a6f1a");
-        System.out.println("556af5asfas56f1a6f1a");
-        System.out.println("556af5asfas56f1a6f1a");
->>>>>>> Sprint_666_12
+       System.out.println("Day one");
     }
 }
